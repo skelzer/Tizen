0a5f457a7bfc7c055a1157dc5bd5bf34bdf05e81
Axl Nunez <axltico12@gmail.com>
2025-12-26 16:19:13 -0500

    Add build packaging task to gulpfile and update package.json scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,16 @@
 import gulp from 'gulp';
 import { deleteAsync as del } from 'del';
+import { readFileSync } from 'fs';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 console.info('Building Moonfin Tizen app');
+
+// Read version from package.json
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+const version = pkg.version;
 
 // Clean the build directory
 function clean() {
@@ -22,8 +31,20 @@ function copyFiles() {
     .pipe(gulp.dest('build/'));
 }
 
+// Package the build into a versioned .wgt file
+async function packageWgt() {
+    const wgtName = `Moonfin-${version}.wgt`;
+    await del([wgtName]);
+    console.info(`Creating ${wgtName}...`);
+    await execAsync(`cd build && zip -r ../${wgtName} . -x "*.git*"`);
+    console.info(`Package created: ${wgtName}`);
+}
+
 // Default build task
 const build = gulp.series(clean, copyFiles);
 
-export { clean, copyFiles };
+// Build and package task
+const buildPackage = gulp.series(clean, copyFiles, packageWgt);
+
+export { clean, copyFiles, packageWgt, buildPackage };
 export default build;
